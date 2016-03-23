382eadf1706462895704ccd7e572ee4519cd54be
Kirill Marchenko <kir.64@ya.ru>
2016-03-23 23:56:45 +0300

    Changes after review

--- pyproxyhunter.py
+++ pyproxyhunter.py
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
+"""Python proxy hunter is a tool that will help you to get quickly a number
+of free and fast proxy servers.
+"""
+import os
 import re
-from collections import namedtuple
-from datetime import datetime
-from threading import activeCount, Thread
+import socket
 
 import requests
-import socket
-from lxml import html
+
+from collections import namedtuple
+from datetime import datetime
+from requests.exceptions import ConnectionError, Timeout
+from threading import activeCount, Thread
 from time import sleep
 from urlparse import urlparse
 
+from lxml import html
+
+
+def get_proxy_object():
+    """Get a Proxy object to store server address and server country"""
+    return namedtuple("Proxy", ["server", "country"])
+
 
 class ProxyHunter(object):
+    """This class search proxy files in google and check every proxy
+    if it's alive. After that, all fresh proxy with country names will be
+    returned and can be stored to file.
     """
-    This class search proxy files in google and then check every proxy if it's alive.
-    After that, all fresh proxy with country names will be returned and can be stored to file.
-    """
-    def __init__(self, verbose=False, store=False, timeout=2, threads=200, pages=1, output_file=None):
-        """
-        :param verbose: bool, print various information about hunting process
+
+    def __init__(self, verbose=False, store=False, timeout=2, threads=500,
+                 pages=1, output_file=None):
+        """:param verbose: bool, print various information about hunting process
         :param store: bool, save founded good proxies to file
-        :param timeout: int, connection timeout via proxy
+        :param timeout: int, connection timeout via proxy in seconds
         :param threads: int, threads to check proxies
-        :param pages: int, google pages to search *.txt files with proxy servers
+        :param pages: int, number of google pages to search for files with proxy servers
         :param output_file: string, filename to save proxies
         """
         self.verbose = verbose
@@ -31,29 +44,33 @@ class ProxyHunter(object):
         self.threads = threads
         self.max_pages_to_search = pages
         if self.store:
-            self.output_file = output_file or 'output.txt'
+            self.output_file = os.path.abspath(output_file or "output.txt")
+            store_dir = os.path.dirname(self.output_file)
+            if not os.access(store_dir, os.W_OK):
+                print "Can't save proxy servers to {} - not enough permissions.".format(self.output_file)
+                exit(1)
+            else:
+                print "Proxy servers would be saved to {}.".format(self.output_file)
 
-        self.Proxy = namedtuple('Proxy', ['server', 'country'])
+        self.proxy = get_proxy_object()
 
     def print_if_verbose(self, message):
-        """
-        Would print a message if self.verbose is True
+        """Would print a message if self.verbose is True
         :param message: string, message to print
         """
         if self.verbose:
             print message
 
     def collect_proxies(self):
-        """
-        Method to search in google for *.txt files with proxy servers
+        """Method to search in google for *.txt files with proxy servers
         :return: list of file urls
         """
         proxies = []
         for page in xrange(self.max_pages_to_search):
             response = requests.get(
-                    'https://www.google.com/search?q=+":8080" +":3128" +":80" filetype:txt&start={}0'.format(page)
+                    "https://www.google.com/search?q=+\":8080\" +\":3128\" +\":80\" filetype:txt&start={}0".format(page)
             )
-            urls = html.fromstring(response.text).xpath('.//*[@id="ires"]//a/@href')
+            urls = html.fromstring(response.text).xpath(".//*[@id=\"ires\"]//a/@href")
             for url in urls:
                 # This monster regexp extracts link to *.txt file with proxy servers
                 proxy_file = re.findall("/url\?q=((?!.*webcache)(?:(?:https?|ftp)://|www\.|ftp\.)[^'\r\n]+\.txt)", url)
@@ -61,8 +78,7 @@ class ProxyHunter(object):
         return proxies
 
     def get_proxies(self, urls):
-        """
-        Requests file, extracts proxy servers from it.
+        """Requests file, extracts proxy servers from it.
         :param urls: list with file urls
         :return: list with collected proxy servers
         """
@@ -70,77 +86,70 @@ class ProxyHunter(object):
         for url in urls:
             result = urlparse(url)
             if not result.scheme:
-                url = 'http://%s' % url
+                url = "http://{}".format(url)
             if result.scheme == 'ftp':
                 # We don't work with FTP
                 return []
-            self.print_if_verbose("Parsing %s" % url)
+            self.print_if_verbose("Parsing {}".format(url))
 
             try:
                 response = requests.get(url, timeout=self.timeout)
-            except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, socket.timeout):
-                self.print_if_verbose("Can't connect to %s" % url)
+            except (ConnectionError, Timeout, socket.timeout):
+                self.print_if_verbose("Can't connect to {}".format(url))
                 return []
             if not response.ok:
-                self.print_if_verbose("Can't connect to %s, status code: %d" % (url, response.status_code))
+                self.print_if_verbose("Can't connect to {}, status code: {}".format(url, response.status_code))
                 return []
             proxies += re.findall('\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}\s*?[:]\s*?\d{1,5}', response.text)
-            self.print_if_verbose("%d proxies received from %s \n" % (len(proxies), url))
+            self.print_if_verbose("{} proxies received from {} \n".format(len(proxies), url))
         return proxies
 
-    def proxy_is_alive(self, server):
-        """
-        Test the ability to connect to website via proxy server
+    def get_proxy_info(self, server):
+        """Test the ability to connect to website via proxy server
         :param server: proxy server to check
-        :return: False in case of any troubles, self.Proxy object if proxy server is good
+        :return: None in case of any troubles, self.proxy object if proxy server is good
         """
         proxies = {
-            "http": 'http://%s/' % server.replace(' ', '')
+            "http": "http://{}/".format(server.replace(' ', ''))
         }
 
         try:
-            response = requests.get('http://ip-api.com/json/?fields=country,status',
+            response = requests.get("http://ip-api.com/json/?fields=country,status",
                                     proxies=proxies, timeout=self.timeout)
-        except Exception as e:
-            self.print_if_verbose(e.message)
+            info = response.json()
+        except Exception as exception:
+            self.print_if_verbose(exception.message)
             # Seems like something went wrong with connection.
             # Probably just dead or slow proxy, so
-            return False
+            return None
 
-        try:
-            info = response.json()
-        except ValueError:
-            return False
-
-        if not info['status'] == 'fail':
-            country = info['country']
-            return self.Proxy(server, country)
+        if not info["status"] == "fail":
+            country = info["country"]
+            return self.proxy(server, country)
         else:
-            return False
+            return None
 
     def check_proxy(self, proxy_to_check, proxy_list):
-        """
-        Check if proxy is good, if so - save it to list for good proxies
+        """Check if proxy is good, if so - save it to list for good proxies
         :param proxy_to_check: proxy server to check
         :param proxy_list: list for good proxies
         """
-        result = self.proxy_is_alive(proxy_to_check)
+        result = self.get_proxy_info(proxy_to_check)
 
-        if result:
-            self.print_if_verbose("%s is alive" % proxy_to_check)
+        if result is not None:
+            self.print_if_verbose("{} is alive".format(proxy_to_check))
             proxy_list.append(result)
         else:
-            self.print_if_verbose("%s is dead" % proxy_to_check)
+            self.print_if_verbose("{} is dead".format(proxy_to_check))
 
     def check_proxies_multi_thread(self, proxies_to_check):
-        """
-        Get a list of proxies and test it with a number of threads.
+        """Get a list of proxies and test it with a number of threads.
         :param proxies_to_check: list with collected proxy servers
         :return: list of live and fast proxies
         """
         proxies = []
-        print '%d proxy to check with %d threads. Please wait.' % (len(proxies_to_check),
-                                                                   self.threads)
+        print "{} proxy to check with {} threads. Please wait.".format(len(proxies_to_check),
+                                                                       self.threads)
         start_time = datetime.now()
 
         for proxy in proxies_to_check:
@@ -150,34 +159,32 @@ class ProxyHunter(object):
                 thread = Thread(target=self.check_proxy, args=(proxy, proxies))
                 thread.daemon = True
                 thread.start()
-            except Exception as e:
-                print 'Exception: %s \nActive threads: %d' % (e, activeCount())
+            except Exception as exception:
+                print "Exception: {} \nActive threads: {}".format(exception, activeCount())
 
         while activeCount() > 1:
             sleep(1)
-        finish_time = datetime.now()
-        delta = (finish_time - start_time).seconds
-        print 'Checked for {} seconds (about {} minutes). {} proxies are good.'.format(delta,
+
+        delta = (datetime.now() - start_time).seconds
+        print "Checked for {} seconds (about {} minutes). {} proxies are good.".format(delta,
                                                                                        round(delta / 60., 1),
                                                                                        len(proxies))
         return proxies
 
     def save_results(self, proxy_list_to_store):
-        """
-        Saves good proxies to file, ordered by country name
-        :param proxy_list_to_store: list with self.Proxy objects
+        """Saves good proxies to file, ordered by country name
+        :param proxy_list_to_store: list with self.proxy objects
         """
         proxy_list_to_store.sort(key=lambda data: data.country)
 
-        with open(self.output_file, 'w') as good_proxy:
+        with open(self.output_file, "w") as good_proxy:
             for proxy in proxy_list_to_store:
-                good_proxy.write('%s\t%s\n' % (proxy.server, proxy.country))
-            print "%d fresh proxies saved to %s" % (len(proxy_list_to_store), self.output_file)
+                good_proxy.write("{}\t{}\n".format(proxy.server, proxy.country))
+            print "{} fresh proxies saved to {}".format(len(proxy_list_to_store), self.output_file)
 
     def hunt(self):
-        """
-        Global method to collect, test, optionally save and return good proxies
-        :return: list of self.Proxy objects
+        """Global method to collect, test, optionally save and return good proxies
+        :return: list of self.proxy objects
         """
         proxies = self.collect_proxies()
         live_proxies = self.check_proxies_multi_thread(proxies)
@@ -193,6 +200,6 @@ class ProxyHunter(object):
         return live_proxies
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     hunter = ProxyHunter(store=True)
     hunter.hunt()
