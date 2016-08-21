67f00b6735bb03ad987431468676ee6bf3da4210
Kirill Marchenko <kir.64@ya.ru>
2016-08-21 19:26:06 +0300

    Use tqdm instead of progressbar. Also, KeyboardInterrupt handling added.

--- pyproxyhunter.py
+++ pyproxyhunter.py
@@ -7,7 +7,6 @@ import os
 import re
 import socket
 
-import progressbar
 import requests
 
 from collections import namedtuple
@@ -15,13 +14,98 @@ from threading import activeCount, Thread
 from time import sleep
 from urlparse import urlparse
 
+import sys
 from lxml import html
+from tqdm import tqdm, TqdmDeprecationWarning
 from requests.exceptions import ConnectionError, Timeout
 
 
 def get_proxy_object():
     """Get a Proxy object to store server address and server country"""
     return namedtuple("Proxy", ["server", "country"])
+
+
+class MyProgressbar(tqdm):
+    def __init__(self, proxy_list, **kwargs):
+        super(MyProgressbar, self).__init__(**kwargs)
+        self.proxy_list = proxy_list
+
+    def get_number_of_good_proxies(self):
+        return len(self.proxy_list)
+
+    def update(self, n=1):
+        # Overriding this method to print number of good proxies already found.
+        """
+        Manually update the progress bar, useful for streams
+        such as reading files.
+
+        Parameters
+        ----------
+        n  : int
+            Increment to add to the internal counter of iterations
+            [default: 1].
+        """
+        if self.disable:
+            return
+
+        if n < 0:
+            raise ValueError("n ({0}) cannot be negative".format(n))
+        self.n += n
+
+        if self.n - self.last_print_n >= self.miniters:
+            # We check the counter first, to reduce the overhead of time()
+            delta_t = self._time() - self.last_print_t
+            if delta_t >= self.mininterval:
+                cur_t = self._time()
+                delta_it = self.n - self.last_print_n  # should be n?
+                elapsed = cur_t - self.start_t
+                # EMA (not just overall average)
+                if self.smoothing and delta_t:
+                    self.avg_time = delta_t / delta_it \
+                        if self.avg_time is None \
+                        else self.smoothing * delta_t / delta_it + \
+                             (1 - self.smoothing) * self.avg_time
+
+                if not hasattr(self, "sp"):
+                    raise TqdmDeprecationWarning("""\
+Please use `tqdm_gui(...)` instead of `tqdm(..., gui=True)`
+""", fp_write=getattr(self.fp, 'write', sys.stderr.write))
+
+                if self.pos:
+                    self.moveto(self.pos)
+
+                # Print bar's update
+                bar = self.format_meter(self.n, self.total, elapsed,
+                                        (self.dynamic_ncols(self.fp) if self.dynamic_ncols else self.ncols), self.desc,
+                                        self.ascii, self.unit, self.unit_scale,
+                                        1 / self.avg_time if self.avg_time else None, self.bar_format)
+                proxies_found = " Collected good proxies: {}".format(self.get_number_of_good_proxies())
+                final_bar = bar + proxies_found
+                self.sp(final_bar)
+
+                if self.pos:
+                    self.moveto(-self.pos)
+
+                # If no `miniters` was specified, adjust automatically to the
+                # maximum iteration rate seen so far.
+                # e.g.: After running `tqdm.update(5)`, subsequent
+                # calls to `tqdm.update()` will only cause an update after
+                # at least 5 more iterations.
+                if self.dynamic_miniters:
+                    if self.maxinterval and delta_t > self.maxinterval:
+                        self.miniters = self.miniters * self.maxinterval \
+                                        / delta_t
+                    elif self.mininterval and delta_t:
+                        self.miniters = self.smoothing * delta_it \
+                                        * self.mininterval / delta_t + \
+                                        (1 - self.smoothing) * self.miniters
+                    else:
+                        self.miniters = self.smoothing * delta_it + \
+                                        (1 - self.smoothing) * self.miniters
+
+                # Store old values for next call
+                self.last_print_n = self.n
+                self.last_print_t = cur_t
 
 
 class ProxyHunter(object):
@@ -54,7 +138,6 @@ class ProxyHunter(object):
                 print "Proxy servers would be saved to {}.".format(self.output_file)
 
         self.proxy = get_proxy_object()
-        self.processed_proxies = 0
 
     def print_if_verbose(self, message):
         """Would print a message if self.verbose is True
@@ -144,8 +227,7 @@ class ProxyHunter(object):
         """
         result = self.get_proxy_info(proxy_to_check)
 
-        self.processed_proxies += 1
-        progress_bar.update(self.processed_proxies)
+        progress_bar.update()
 
         if result is not None:
             self.print_if_verbose("{} is alive".format(proxy_to_check))
@@ -159,32 +241,35 @@ class ProxyHunter(object):
         :return: list of live and fast proxies
         """
         proxies = []
-        print "{} proxy to check with {} threads. Please wait.".format(len(proxies_to_check),
-                                                                       self.threads)
+        total_collected = len(proxies_to_check)
+        print "\n{} proxy to check with {} threads. Please wait.\n".format(total_collected, self.threads)
         threads = []
-
-        bar = progressbar.ProgressBar(maxval=float(len(proxies_to_check)), widgets=[
-            progressbar.ETA(),
-            " Progress: ",
-            progressbar.Percentage(),
-        ]).start()
+        bar_format = "{l_bar}{bar}| {n_fmt}/{total_fmt} ETA:{remaining}"
+        check_bar = MyProgressbar(total=total_collected, desc="Check progress", bar_format=bar_format,
+                                  proxy_list=proxies, ncols=80)
+        check_bar.clear()
 
         for proxy in proxies_to_check:
             while activeCount() == self.threads:
                 sleep(1)
             try:
-                thread = Thread(target=self.check_proxy, args=(proxy, proxies, bar))
+                thread = Thread(target=self.check_proxy, args=(proxy, proxies, check_bar))
                 thread.daemon = True
-                threads.append(thread)
                 thread.start()
+                threads.append(thread)
+            except KeyboardInterrupt:
+                print "\nOh, you want to break checking? " \
+                      "Let me stop running threads and give you everything I've found."
+                break
             except Exception as exception:
                 print "Exception: {} \nActive threads: {}".format(exception, activeCount())
 
         for thread in threads:
             thread.join()
-        bar.finish()
 
-        print "{} proxies are good.".format(len(proxies))
+        check_bar.close()
+
+        # print "{} proxies are good.".format(len(proxies))
         return proxies
 
     def save_results(self, proxy_list_to_store):
@@ -207,7 +292,8 @@ class ProxyHunter(object):
 
         if not live_proxies:
             print "It's weird, but no live proxy was found. " \
-                  "Please, check if http://ip-api.com/ live now, and contact the developer."
+                  "If you didn't press Ctrl+C, then, please, check if http://ip-api.com/ live now, " \
+                  "and contact the developer."
             exit(1)
 
         if self.store:
